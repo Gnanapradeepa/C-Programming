d0e261744ce98111f35f89a12df9493e15f6da1c
Gnanapradeepa <34647153+Gnanapradeepa@users.noreply.github.com>
2017-12-19 14:38:25 +0530

    Update leap year or not.c

--- CodeKata/leap year or not.c
+++ CodeKata/leap year or not.c
@@ -1,14 +1,11 @@
-#include <stdio.h>
-int main(void) {
-	int year;
-	scanf("%d",&year);
-	if(year%4==0)
-	{
-		printf("leap year");
-	}
-	else
-	{
-		printf("not a leap year");
-	}
-	return 0;
+#include<stdio.h>
+int main(void){
+    int year;
+    scanf("%d",&year);
+    if(((year%4==0)&&(year%100!=0))||(year%400==0))
+         printf("%d is a leap year",year);
+    else
+         printf("%d is not a leap year",year);
+  
+    return 0;
 }
