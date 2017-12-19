6f81fc5c9ce93245f5d66fe922e278502a326ac8
Gnanapradeepa <34647153+Gnanapradeepa@users.noreply.github.com>
2017-12-19 15:03:17 +0530

    Create sum of N using for loop.c

---
+++ CodeKata/sum of N using for loop.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(void){
+    int sum=0,n,i;
+    int a[30];
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+    	scanf("%d",&a[i]);
+    	sum=sum+a[i];
+    }
+  printf("%d",sum);
+    return 0;
+}
