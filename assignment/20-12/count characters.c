bed510fc8091101a731a0ea0a2f392412a264862
Gnanapradeepa <34647153+Gnanapradeepa@users.noreply.github.com>
2017-12-26 22:40:41 +0530

    Update count characters.c

--- assignment/20-12/count characters.c
+++ assignment/20-12/count characters.c
@@ -1,23 +1,27 @@
-#include<stdio.h>
-int main(void)
-{
-char a[10];
-int count,i,j,k;
-scanf("%s",&a);
-for(i=0;a[i]!='\0';i++)
-{
-count=1;
-for(j=i+1;a[j]!='\0';j++)
-{
-if(a[i]==a[j])
-{
-count++;
-for(k=j+1;a[k]!='\0';k++) 
-a[k-1]=a[k];
-a[k-1]='\0';
-}
-}
-printf("%c - %d\n",a[i],count);
-}
-return 0;
+#include <stdio.h>
+
+int main(void) {
+	int i,j,c=0,l;
+	char a[100];
+	int b[10];
+	gets(a);
+	puts(a);
+	l=strlen(a);
+	for(i=0;i<l;i++)
+	{
+		b[i]=1;
+		for(j=i+1;j<l;j++)
+		{
+			if(a[i]==a[j])
+			{
+			  b[i]++;
+			  a[j]=a[j+1];
+			  l--;
+			  
+			}
+		}
+			printf("%c=%d\n",a[i],b[i]);
+	}
+
+	return 0;
 }
